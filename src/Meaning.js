5f986b9a66eb73e18cc87e7c49d7a469ec30b793
Martinaroque <martirocklx@hotmail.com>
2023-07-31 18:17:53 +0100

    correct an erro

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,20 +5,19 @@ export default function  Meaning (props){
  console.log(props.meaning)
  
     return(
- <div className="meaning">
+ <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definition.map(function(definition,index){
+            {props.meaning.definitions.map(function(definition,index){
 return (
 
     <div key={index}>
         <p>
     {definition.definition}
     <br />
-   <em>{definition.example}</em> 
+   <strong><em>{definition.example}</em> </strong>
     </p>
     </div>
-)
-
+);
             })}
      </div>   
         
