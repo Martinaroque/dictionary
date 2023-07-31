27fbb41edbba6fb60501c8b74bdc4fbfd9f9ce66
Martinaroque <martirocklx@hotmail.com>
2023-07-31 18:09:51 +0100

    adding content to results

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,23 @@ export default function  Meaning (props){
  console.log(props.meaning)
  
     return(
-        "hello bitch"
+ <div className="meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            {props.meaning.definition.map(function(definition,index){
+return (
+
+    <div key={index}>
+        <p>
+    {definition.definition}
+    <br />
+   <em>{definition.example}</em> 
+    </p>
+    </div>
+)
+
+            })}
+     </div>   
+        
    );
       
 }
